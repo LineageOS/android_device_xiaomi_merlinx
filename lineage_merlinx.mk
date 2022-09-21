09cdb169abbc73f77dad60340d71817ceeb77d2b
Matsvei Niaverau <matvej2002@gmail.com>
2022-09-21 21:22:13 +0100

    merlinx: Override fingerprint and build description from V12.5.4.0

--- lineage_merlinx.mk
+++ lineage_merlinx.mk
@@ -21,3 +21,8 @@ PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 9
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="merlin-user 11 RP1A.200720.011 V12.5.4.0.RJOMIXM release-keys"
+
+BUILD_FINGERPRINT := Redmi/merlin/merlin:11/RP1A.200720.011/V12.5.4.0.RJOMIXM:user/release-keys
