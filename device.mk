a44a30bfa57b0a3413090e47ab6b425f03554d00
Dan Pasanen <dan.pasanen@gmail.com>
2022-09-26 15:42:15 +0100

    mt6768-common: Exclude lineage-sdk overlays from RRO
    * lineage-sdk overlays don't work with RRO

--- device.mk
+++ device.mk
@@ -35,6 +35,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage
+
 # Rootdir
 PRODUCT_PACKAGES += \
     init.project.rc
