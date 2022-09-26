c5f0a9e29502dfe71e3a7e2e68c8186687e621aa
bengris32 <bengris32@protonmail.ch>
2022-09-26 15:45:00 +0100

    merlinx: Convert overlays into Runtime Resource Overlays
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -32,11 +32,14 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
+
+PRODUCT_PACKAGES += \
+    FrameworksResOverlayMerlinx \
+    SystemUIOverlayMerlinx
 
 # Rootdir
 PRODUCT_PACKAGES += \
