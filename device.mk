87c3432aa0fa4c7c79b4e28c388a063afc7b1d01
Matsvei Niaverau <matvej2002@gmail.com>
2023-02-27 00:03:35 +0100

    merlinx: Decommonize Aperture overlay
    * Macro camera-id is different on both devices
    
    Change-Id: If7116adef881698a931f07f9b8056587b2b37088

--- device.mk
+++ device.mk
@@ -41,6 +41,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_merlinnfc/com.android.nfc_extras.xml
 
 # Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage
+
 PRODUCT_PACKAGES += \
     FrameworksResOverlayMerlinx \
     SystemUIOverlayMerlinx
